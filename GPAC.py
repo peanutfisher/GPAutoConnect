f492139c71298c0e867447568cd590d62f3703aa
peanutfisher <meifajia@outlook.com>
2023-12-26 15:58:24 +0800

    It is V3.0

--- GPAC.py
+++ GPAC.py
@@ -53,142 +53,146 @@ def check_config(filename):
     
     return [SCALE_RATE, NT_PASSWD, RSA_PASSWD]
 
-
-def click_center(png, x_offset=0, y_offset=0, showed=True):
+def click_center(image, click=True, wait_time=1, num=0.96, repeated=True):
     """
     Used to click the center of a picture and also provide following function:
-    1. Use x_offset,y_offset to click somewhere around center position,
-    2. Use "showed" flag to mark if need to wait for the ICON/PROMPT showup before click it.
-    3. Return the center position(x,y).
+    1. check if the image is there and return location(x,y)
+    2. waiting a moment to see if the image show up
+    3. in case image not found, decrease the confidence to get better recognition
+    
     """
     try:
-        # Use to records how long we are waiting for prompt/icon showup
-        number = 0
-        # first check if need to wait ICON/PROMPT showup
-        # if ICON/PROMPT is there then get its position
-        if showed:
-            x1, y1 = pg.locateCenterOnScreen(png, confidence=0.96)
-        else:
-            # if ICON/PROMPT is not there then waiting it showup
-            while (not showed):
-                showed = pg.locateCenterOnScreen(png, confidence=0.96)
-                number += 1
-                time.sleep(1)
-                print(f'Waiting {png} for {number} times')
-                # in case we wait too much time
-                if number > 60:
-                    print('Loging process could be hung, clearing unknown status...')
-                    number = 0
-                    clear_status()
-            x1, y1 = showed[0], showed[1]
- 
-        # click the center position or somewhere around it
-        pg.click(x1+x_offset, y1+y_offset, button='left')
+        location = None
+        counter = 0
+        
+        while (not location):
+            location = pg.locateCenterOnScreen(image, minSearchTime=wait_time, confidence=num, grayscale=True)
+            
+            # reduce confidence to get better recogniztion
+            num -= 0.01
+            counter += 1
+            print(f'Waiting {image} for {counter} times')
+            
+            if (not repeated) and counter == 3:
+                return False
+            # in case we can not find the image then retry the process
+            if num < 0.80:
+                counter = 0
+                raise
+        
+        # click the center position
+        if click:
+            pg.click(location[0], location[1])
+        
         # return back center position
-        print(f'{png} position({x1},{y1})')
-        return (x1, y1)
+        print(f'{image} position({location[0]},{location[1]})')
+        return (location[0], location[1])
     
     except Exception as e:
-        print(f'Cannot find {png} in your Screen.')
+        print(f'Cannot find {image} in your Screen.')
+        # clear_status()
 
-
-
-def signin(temp=None):
-    # move mouse to input login passwd info
-    x1, y1 = click_center('GP_login_signon.png', 0, -85, showed=False)
-    # check if it is NT login or RSA login
-    if temp:
-        pg.write(temp)
-    else:
+def signin(NT_PW, TOKEN):
+    # Get the RSA code firstly
+    get_rsa(TOKEN)
+    
+    # NT sign in
+    if click_center('GP_nt_input.png', repeated=False):
+        pg.write(NT_PW)
+        click_center('GP_login_signon.png', click=True)
+        print('NT password inputed')
+        time.sleep(5)
+    
+    # RSA sign in
+    if click_center('GP_rsa_input.png', repeated=False):   
         pg.hotkey('ctrl', 'v')
-    # click "sign on" button
-    pg.doubleClick(x1, y1, button='left', duration=0.3)
-    time.sleep(2)
+        click_center('GP_login_signon.png', click=True)
+        print('RSA code inputed')
 
+    else:
+        # It may need to input username manually
+        pass
+        print('Signin failure, check login page')
 
-def get_rsa(token):
+def get_rsa(TOKEN):
     """
     Get RSA Credential
     """
     # Click RSA icon to show RSA main window
-    if not click_center('RSA_icon1.png'):
-        click_center('RSA_icon2.png')
-    pg.moveTo(0,500)
-    time.sleep(3)    
+    if click_center('RSA_icon.png'):
+        pg.moveTo(0,500)
+        time.sleep(2)    
+    
     # Check if the RSA passcode is already being inputed, if not then get a new one
-    if not click_center('RSA_copy.png'):
-        if not click_center('RSA_main.png'):
-            click_center('RSA_main1.png')
-        pg.typewrite(token)
+    if click_center('RSA_main.png', repeated=False):
+        pg.write(TOKEN)
         pg.press('enter')
         time.sleep(1)
-        # click copy button
-        click_center('RSA_copy.png')
+    
+    click_center('RSA_copy.png')
 
-    # Change focus to browser window in case RSA window cover it
-    pg.hotkey('alt', 'tab')
+    # Change focus to browser window in case RSA window covered it
+    pg.click(540, 960)
 
-
-def connect_GP(passwd1, passwd2):
+def connect_GP(NT_PW, TOKEN):
     """
     Connect GP
     """
     # find and click "connect"
-    if not click_center('GP_connect.png'):
-        click_center('GP_connect1.png')
-    time.sleep(5)
-    # Press hotkey "WIN+UP" in case browser not maximum
-    pg.hotkey('win', 'up')
-    # check if GP is reconnected or connecting
-    reconnect = False
-    connecting = False
-    count = 0
-    while (not (reconnect or connecting)):
-        # keep querying if reconnected
-        reconnect = pg.locateOnScreen('GW_change.png', confidence=0.95)
-        connecting = pg.locateOnScreen('GP_login_signon.png', confidence=0.95)
-        
-        # record retry times
-        count += 1
-        print(f'reconnect: {reconnect}, connecting: {connecting}, check: {count}times')
-        
-        time.sleep(1)
-        # if stuck then do cleaning
-        if count > 60:
-            print('Something Stuck and cannot move...')
-            count = 0
-            clear_status()
-            
-    # If it is connected after click "connect" goto choose_GP func
-    if reconnect:
-        choose_GP()
-        os._exit(0)
-    else:
-        signin(passwd1)
-        get_rsa(passwd2)
-        # GP login with RSA
-        signin()
+    if click_center('GP_connect.png', repeated=False):
+        time.sleep(8)
+        # Press hotkey "WIN+UP" in case browser not maximum
+        pg.hotkey('win', 'up')
 
+        # check if GP is reconnected or connecting
+        reconnect = False
+        connecting = False
+        count = 0
+        while (not (reconnect or connecting)):
+            # keep querying if reconnected
+            reconnect = click_center('GW_change.png', click=False, repeated=False)
+            connecting = click_center('GP_login_signon.png', click=False, repeated=False)
+            
+            # record retry times
+            count += 1
+            print(f'reconnect: {reconnect}, connecting: {connecting}, check: {count}times')
+            
+            if count > 10:
+                print('Something Stuck and cannot move...')
+                count = 0
+                #clear_status()
+            
+        # If it is connected after click "connect" goto choose_GP func
+        if reconnect:
+            choose_GP()
+            
+        else:
+            signin(NT_PW, TOKEN)
+
+    else:
+        choose_GP()
+        print('Connet_GP failure')  
+        
 def choose_GP():
     # check if no-split is connected
-    if (not pg.locateOnScreen('GP_nosplit.png', confidence=0.95)):
+    if (not click_center('GP_nosplit.png', repeated=False)):
         print('changing gateway')
-        # click the "Change Gateway", 2 types(lose focus and with focus)
-        if not click_center('GW_change.png'):
-            click_center('GW_change1.png')
-        if pg.locateOnScreen('GP_choose.png', confidence=0.95):
-            # Use "Tab" key to redirect focus on the input area
+        # click the "Change Gateway"
+        click_center('GW_change.png')
+        time.sleep(1)
+        
+        if click_center('GP_choose.png', repeated=False):
+            # Search No split channel and connecting
+            pg.typewrite('no')
             pg.press('tab')
-        # Search No split channel and connecting
-        pg.typewrite('no')
-        pg.press('tab')
-        pg.press('enter')
+            pg.press('enter')
         # Waiting for few seconds until connected
         time.sleep(10)
-        click_center('GP_nosplit.png', showed=False)
+        click_center('GP_nosplit.png')
     
     print('GP is connected! Enjoy it!')
     # refresh_web()
+    os._exit(0)
 
 def clear_status():
     """
@@ -213,7 +217,7 @@ def refresh_web():
     os.system('start https://rcmappprd001.corp.emc.com/rcm/RCM_Schedule/schedule.php')
     time.sleep(5)
     # waiting for webpage loading complete and click home button
-    click_center('RCM_page.png', 1000, 70, showed=False)
+    click_center('RCM_page.png', repeated=False)
     print('RCM page loading ok!')
 
 def dontsleep():
@@ -222,10 +226,10 @@ def dontsleep():
     """
     print('Starting Dontsleep...')
     # get the dontsleep icon
-    pos = pg.locateCenterOnScreen('dontsleep.png', confidence = 0.90)
+    pos = click_center('dontsleep.png', click=False)
     pg.rightClick(pos[0], pos[1])
     # enable the icon if we disabled
-    if click_center('dontsleep_off.png'):    
+    if click_center('dontsleep_off.png',repeated=False):    
         print('Enable the Dontsleep.')
     else:
         print('Dontsleep is ON.')
@@ -237,7 +241,7 @@ def main():
     SCALE_RATE = secret[0]
     NT_PASSWD = secret[1]
     RSA_PASSWD = secret[2]
-    print([SCALE_RATE, NT_PASSWD, RSA_PASSWD])
+    #print([SCALE_RATE, NT_PASSWD, RSA_PASSWD])
     # The pictures are inside different folders based on resolution size
     os.chdir(os.path.join(os.getcwd(), SCALE_RATE))
     print(os.getcwd())
@@ -247,24 +251,31 @@ def main():
     
     print('Starting GP...')
     
-    connect_flag = (pg.locateOnScreen('GP_login_signon.png', confidence=0.95)) or (pg.locateOnScreen('GP_connect1.png', confidence=0.95))
-    if not connect_flag:
-        # check if GP icon is gray(notconnected or connectfailed)
-        gray_gp = click_center('gray_GP1.png', showed=True) or click_center('gray_GP2.png', showed=True)
-    blue_gp = click_center('GP_blue.png')
-    # if not connected then do connection flow
-    # adding situation that GP icon if blinking for connecting
-    if connect_flag or gray_gp :
+    # scenario 1: If GP is connected
+    if click_center('GP_blue.png', repeated=False):
+        choose_GP()
+    
+    # Scenario 2: click gray GP icon for 1st connection or there is a GP window there for connection
+    elif click_center('gray_GP1.png', repeated=False) or click_center('gray_GP2.png', repeated=False) or click_center('GP_connect.png', click=False, repeated=False):
         connect_GP(NT_PASSWD, RSA_PASSWD)
         time.sleep(15)
-        click_center('GP_blue.png', 0, 0, showed=False)
+        if not click_center('GW_change.png', repeated=False):
+            click_center('GP_blue.png')
         choose_GP()
-    # if connected then check if no-split is choosen
-    elif (blue_gp):
+    
+    # Scenario 3: If the blink gp showed(connecting)
+    elif click_center('GP_login_signon.png', click=False, repeated=False):
+        signin(NT_PASSWD, RSA_PASSWD)
+        time.sleep(15)
+        if not click_center('GW_change.png', repeated=False):
+            click_center('GP_blue.png')
         choose_GP()
-    # If the blink gp showed
+    
+    # The worst scenario - cannot connect
     else:
-        clear_status()
+        #clear_status()
+        print('Main process failure')
+    
         
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -7,109 +7,154 @@ from get_screen_size import *
 NT_PASSWD = 'Ballball17'
 RSA_PASSWD = '10538185'
 
-def click_center(png, x_offset=0, y_offset=0, showed=True):
+def click_center(image, click=True, wait_time=1, num=0.96, repeated=True):
     """
     Used to click the center of a picture and also provide following function:
-    1. Use x_offset,y_offset to click somewhere around center position,
-    2. Use "showed" flag to mark if need to wait for the ICON/PROMPT showup before click it.
-    3. Return the center position(x,y).
+    1. check if the image is there and return location(x,y)
+    2. waiting a moment to see if the image show up
+    3. in case image not found, decrease the confidence to get better recognition
+    
     """
     try:
-        # Use to records how long we are waiting for prompt/icon showup
-        number = 0
-        # first check if need to wait ICON/PROMPT showup
-        # if ICON/PROMPT is there then get its position
-        if showed:
-            x1, y1 = pg.locateCenterOnScreen(png)
-        else:
-            # if ICON/PROMPT is not there then waiting it showup
-            while (not showed):
-                showed = pg.locateCenterOnScreen(png)
-                number += 1
-                print(f'Waiting {png} for {number} times')
-                # in case we wait too much time
-                if number > 50:
-                    print('Loging process could be hung, clearing unknown status...')
-                    clear_status()
-            x1, y1 = showed[0], showed[1]
- 
-        # click the center position or somewhere around it
-        pg.click(x1+x_offset, y1+y_offset, button='left')
+        location = None
+        counter = 0
+        
+        while (not location):
+            location = pg.locateCenterOnScreen(image, minSearchTime=wait_time, confidence=num, grayscale=True)
+            
+            # reduce confidence to get better recogniztion
+            num -= 0.01
+            counter += 1
+            print(f'Waiting {image} for {counter} times')
+            
+            if (not repeated) and counter == 3:
+                return False
+            # in case we can not find the image then retry the process
+            if num < 0.80:
+                counter = 0
+                raise
+        
+        # click the center position
+        if click:
+            pg.click(location[0], location[1])
+        
         # return back center position
-        print(f'{png} position({x1},{y1})')
-        return (x1, y1)
+        print(f'{image} position({location[0]},{location[1]})')
+        return (location[0], location[1])
+    
     except Exception as e:
-        print(f'Cannot find {png} in your Screen.')
+        print(f'Cannot find {image} in your Screen.')
+        # clear_status()
 
+def signin(NT_PW, TOKEN):
+    # Get the RSA code firstly
+    get_rsa(TOKEN)
+    
+    # NT sign in
+    if click_center('GP_nt_input.png', repeated=False):
+        pg.write(NT_PW)
+        click_center('GP_login_signon.png', click=True)
+        print('NT password inputed')
+        time.sleep(8)
+    
+    # RSA sign in
+    if click_center('GP_rsa_input.png', repeated=False):   
+        pg.hotkey('ctrl', 'v')
+        click_center('GP_login_signon.png', click=True)
+        print('RSA code inputed')
 
-def get_rsa(token):
+    else:
+        # It may need to input username manually
+        pass
+        print('Signin failure, check login page')
+
+def get_rsa(TOKEN):
     """
     Get RSA Credential
     """
     # Click RSA icon to show RSA main window
-    if not click_center('RSA_icon1.png'):
-        click_center('RSA_icon2.png')
-    pg.moveTo(0,500)    
-    time.sleep(3)
+    if click_center('RSA_icon.png'):
+        pg.moveTo(0,500)
+        time.sleep(2)    
+    
     # Check if the RSA passcode is already being inputed, if not then get a new one
-    if not click_center('RSA_copy.png'):
-        if not click_center('RSA_main.png'):
-            click_center('RSA_main1.png')
-        
-        pg.typewrite(token)
+    if click_center('RSA_main.png', repeated=False):
+        pg.write(TOKEN)
         pg.press('enter')
-        time.sleep(3)
-        # click copy button
-        click_center('RSA_copy.png')
+        time.sleep(1)
+    
+    click_center('RSA_copy.png')
 
     # Change focus to browser window in case RSA window cover it
-    pg.hotkey('alt', 'tab')
+    pg.click(540, 960)
 
+def connect_GP(NT_PW, TOKEN):
+    """
+    Connect GP
+    """
+    # find and click "connect"
+    if click_center('GP_connect.png'):
+        time.sleep(8)
+        # Press hotkey "WIN+UP" in case browser not maximum
+        pg.hotkey('win', 'up')
+
+        # check if GP is reconnected or connecting
+        reconnect = False
+        connecting = False
+        count = 0
+        while (not (reconnect or connecting)):
+            # keep querying if reconnected
+            reconnect = click_center('GW_change.png', click=False, repeated=False)
+            connecting = click_center('GP_login_signon.png', click=False, repeated=False)
+            
+            # record retry times
+            count += 1
+            print(f'reconnect: {reconnect}, connecting: {connecting}, check: {count}times')
+            
+            if count > 10:
+                print('Something Stuck and cannot move...')
+                count = 0
+                #clear_status()
+            
+        # If it is connected after click "connect" goto choose_GP func
+        if reconnect:
+            choose_GP()
+            os._exit(0)
+        else:
+            signin(NT_PW, TOKEN)
+
+    else:
+        pass
+        print('Connet_GP failure')  
+        
 def choose_GP():
     # check if no-split is connected
-    click_center('GP_blue.png', 0, 0, showed=False)
-    if (not pg.locateOnScreen('GP_nosplit.png')):
+    if (not click_center('GP_nosplit.png', repeated=False)):
         print('changing gateway')
         # click the "Change Gateway"
-        click_center('GW_change.png', showed=False)
-        if pg.locateOnScreen('GP_choose.png'):
+        click_center('GW_change.png')
+        time.sleep(1)
+        
+        if click_center('GP_choose.png', repeated=False):
+            # Search No split channel and connecting
+            pg.typewrite('no')
             pg.press('tab')
-        pg.typewrite('no')
-        pg.press('tab')
-        pg.press('enter')
+            pg.press('enter')
+        # Waiting for few seconds until connected
+        time.sleep(10)
+        click_center('GP_nosplit.png')
+    
+    print('GP is connected! Enjoy it!')
+    # refresh_web()
 
-def signin(temp=None):
-    # move mouse to input login passwd info
-    x1, y1 = click_center('GP_login_signon.png', 0, -85, showed=False)
-    # check if it is NT login or RSA login
-    if temp:
-        pg.typewrite(temp)
-    else:
-        pg.hotkey('ctrl', 'v')
-    # click "sign on" button
-    pg.doubleClick(x1, y1, button='left', duration=0.3)
-    time.sleep(2)
-
-def dontsleep():
-    """
-    This is use to start the dontsleep tools.
-    """
-    print('Starting Dontsleep...')
-    # get the dontsleep icon
-    pos = pg.locateCenterOnScreen('dontsleep.png', confidence = 0.90)
-    pg.rightClick(pos[0], pos[1])
-    # enable the icon if we disabled
-    if click_center('dontsleep_off.png'):    
-        print('Enable the Dontsleep.')
-    else:
-        print('Dontsleep is ON.')
-                
 os.chdir(os.path.join(os.getcwd(),'125'))
 #gray_gp = click_center('gray_GP1.png', showed=True) or click_center('gray_GP2.png', showed=True)
 #print(pg.locateCenterOnScreen('GP_connect.png'))
-#print(pg.locateCenterOnScreen('RSA_main.png'))
 
-#signin(NT_PASSWD)
+#loc = click_center('GP_login_input.png', click=False)
+#print(loc)
+
+signin(NT_PASSWD, RSA_PASSWD)
 #get_rsa(RSA_PASSWD)
 #choose_GP()
-dontsleep()
+#dontsleep()
